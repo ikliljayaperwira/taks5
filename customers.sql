39663e68cc5eb477ff5fafbe5889aa028b17e416
ikliljayaperwira <54500877+ikliljayaperwira@users.noreply.github.com>
2020-07-21 20:13:32 +0700

    Add files via upload

---
+++ customers.sql
@@ -0,0 +1,61 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Waktu pembuatan: 21 Jul 2020 pada 15.10
+-- Versi server: 10.4.11-MariaDB
+-- Versi PHP: 7.4.5
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `northwind`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `customers`
+--
+
+CREATE TABLE `customers` (
+  `CustomerID` varchar(5) NOT NULL,
+  `CompanyName` varchar(40) NOT NULL,
+  `ContactName` varchar(30) DEFAULT NULL,
+  `ContactTitle` varchar(30) DEFAULT NULL,
+  `Address` varchar(60) DEFAULT NULL,
+  `City` varchar(15) DEFAULT NULL,
+  `Region` varchar(15) DEFAULT NULL,
+  `PostalCode` varchar(10) DEFAULT NULL,
+  `Country` varchar(15) DEFAULT NULL,
+  `Phone` varchar(24) DEFAULT NULL,
+  `Fax` varchar(24) DEFAULT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indeks untuk tabel `customers`
+--
+ALTER TABLE `customers`
+  ADD PRIMARY KEY (`CustomerID`),
+  ADD KEY `City` (`City`),
+  ADD KEY `CompanyName` (`CompanyName`),
+  ADD KEY `PostalCode` (`PostalCode`),
+  ADD KEY `Region` (`Region`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
